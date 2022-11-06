58bc65b3c568ac261862f5feebcd8ec17b9b103a
unknown <yaribum@mail.ru>
2022-11-07 01:31:38 +0200

    first commit

---
+++ myapp/src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div className='photo_big'>
+                <img src='https://i.pinimg.com/originals/b0/1b/0d/b01b0d9cccee0fb99a67eed4f6e4db6b.jpg' />
+            </div>
+            <div>Ava + d</div>
+            <div>My post
+                <div>New post</div>
+            </div>
+            <div>
+                <div>Post 1</div>
+                <div>Post 2</div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
